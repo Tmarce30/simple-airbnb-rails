13d9b45b910654a526726c2de89ec67e0ab01adc
Thibaud Marcé <thibaud.marce@gmail.com>
2018-02-20 18:08:32 +0100

    added picture column to flat

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180219130504) do
+ActiveRecord::Schema.define(version: 20180220170639) do
 
   create_table "flats", force: :cascade do |t|
     t.string   "name"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20180219130504) do
     t.integer  "number_of_guests"
     t.datetime "created_at",       null: false
     t.datetime "updated_at",       null: false
+    t.string   "picture_url"
   end
 
 end
